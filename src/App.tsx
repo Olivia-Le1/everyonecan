c9a904f0757530ad7ec854726d1b10b4d430ecdb
Olivia-Le1 <l01048666065@gmail.com>
2026-08-01 23:13:02 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+// fixed router
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { HashRouter, Route, Routes } from "react-router-dom";
 import { Toaster as Sonner } from "@/components/ui/sonner";
