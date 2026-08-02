623c2c604385666f03ac826027e50f4063e90ed9
Olivia-Le1 <l01048666065@gmail.com>
2026-08-02 23:17:43 +0900

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -3,17 +3,8 @@ import { Hero } from "@/components/Hero";
 import { Countries } from "@/components/Countries";
 import { Articles } from "@/components/Articles";
 import { Footer } from "@/components/Footer";
-import { useEffect } from "react";
 
 const Index = () => {
-  return (
-    <div>
-      TEST PAGE
-    </div>
-  );
-};
-
-export default Index;
   return (
     <div className="min-h-screen bg-background">
       <Navbar />
