a1dc0939ab8dd60144dd5c59b14d0637eecddf12
Olivia-Le1 <l01048666065@gmail.com>
2026-08-02 23:28:45 +0900

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,9 +1,11 @@
 import { Navbar } from "@/components/Navbar";
+import { Hero } from "@/components/Hero";
 
 const Index = () => {
   return (
     <div>
       <Navbar />
+      <Hero />
     </div>
   );
 };
