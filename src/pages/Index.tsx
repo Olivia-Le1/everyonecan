552339aff16a0bb0a27cd932a53e6189f485d851
Olivia-Le1 <l01048666065@gmail.com>
2026-08-02 23:02:31 +0900

    Debug: Add logging to Index page

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -3,17 +3,24 @@ import { Hero } from "@/components/Hero";
 import { Countries } from "@/components/Countries";
 import { Articles } from "@/components/Articles";
 import { Footer } from "@/components/Footer";
+import { useEffect } from "react";
 
-const Index = () => (
-  <div className="min-h-screen bg-background">
-    <Navbar />
-    <main>
-      <Hero />
-      <Countries />
-      <Articles />
-    </main>
-    <Footer />
-  </div>
-);
+const Index = () => {
+  useEffect(() => {
+    console.log("Index page loaded");
+  }, []);
+
+  return (
+    <div className="min-h-screen bg-background">
+      <Navbar />
+      <main>
+        <Hero />
+        <Countries />
+        <Articles />
+      </main>
+      <Footer />
+    </div>
+  );
+};
 
 export default Index;
